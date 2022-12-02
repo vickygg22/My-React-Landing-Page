d53bba1473620ba7745b0aeaba19857947edd46e
Victoria <victoria1998castellar@hotmail.com>
2022-12-02 19:29:03 +0000

    Version with images

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,43 +1,15 @@
 import React from "react";
 import Images from "../../img/big-project-image.png"
-const Card = () => {
+const Card = (props) => {
     return (
-      <>
-      <div className=" marginLeft row">
       <div className="card col-3 mt-3 border padding">
-        <img src={Images} id="square1" alt=""/>
-        <h4 className=" mt-3 text-dark">Card title</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. ra eusimod odio, gravida pellentesque urna varius vitae.</p>
+        <img src={props.item.imgUrl} className="imageSize" id="square1" alt=""/>
+        <h4 className=" mt-3 text-dark">{props.item.title}</h4>
+        <p>{props.item.description}</p>
         <div className="mt-3 border-top marginBottom">
           <button type="button" className="btn btn-primary">Find Out More!</button>
         </div>
       </div>
-      <div className="card col-3 mt-3 border padding">
-        <img src={Images} id="square2" alt=""/>
-        <h4 className="mt-3 text-dark">Card title</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. ra eusimod odio, gravida pellentesque urna varius vitae.</p>
-        <div className="mt-3 border-top marginBottom">
-          <button type="button" className="btn btn-primary">Find Out More!</button>
-        </div>
-      </div>
-      <div className="card col-3 mt-3 border padding">
-        <img src={Images} id="square3" alt=""/>
-        <h4 className="mt-3 text-dark">Card title</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. ra eusimod odio, gravida pellentesque urna varius vitae.</p>
-        <div className="mt-3 border-top marginBottom">
-          <button type="button" className="btn btn-primary">Find Out More!</button>
-        </div>
-      </div>
-      <div className="card col-3 mt-3 border padding">
-        <img src={Images} id="square3" alt=""/>
-        <h4 className="mt-3 text-dark">Card title</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. ra eusimod odio, gravida pellentesque urna varius vitae.</p>
-        <div className="mt-3 border-top marginBottom">
-          <button type="button" className="btn btn-primary">Find Out More!</button>
-        </div>
-      </div>
-    </div>
-    </>
     )
 }
 export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,15 +5,21 @@ import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
 import Card from "./card.jsx";
 import Footer from "./footer.jsx";
+import data from "./data.json"
 //create your first component
 const Home = () => {
+	const listOfCards = data.map((item) => {
+		return <Card item={item}/>
+	})
 	return (
 		<>
 		<div className="text-center">
 			<Navbar />
 		<div className="container">
 		<Jumbotron />
-		<Card />
+		<div className=" marginLeft row">
+			{listOfCards}
+		</div>
 		</div>
 		<Footer />
 		</div>
